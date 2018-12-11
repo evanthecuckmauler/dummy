91c12f3aecc22e2884195dd74aa83945c5a27565
Evan Verhulst <evanverhulst@amdg.rockhursths.edu>
2018-12-11 11:18:03 -0600

    eat function

--- main.js
+++ main.js
@@ -1,67 +1,75 @@
-var gf;
+var puppy;
 var feed = []; // an empty array
 var numFood = 10;
 
-var a = [1,2,3]
+var a = [1, 2, 3];
+
 function setup() {
-    createCanvas(640,480);
-    gf = new Giraffe;
+    rectMode(CENTER);
+    createCanvas(1024, 768);
+    puppy = new Puppy();
+
     // initializing 10 pieces of food
-    for(var i = 0; i < numFood; i++) {
+    for (var i = 0; i < numFood; i++) {
         feed.push(new Food(random(width), random(height)));
     }
 }
 
 function draw() {
-    background('#FFFAED');
-    gf.display();
-    
+    background(0, 255, 0);
+    puppy.display();
+
     // display all the food
-    for(var i = 0; i < numFood; i++) {
+    for (var i = 0; i < feed.length; i++) {
         feed[i].display();
     }
 }
 
 function mousePressed() {
-    gf.eat();
+    puppy.eat();
 }
 
 function Food(x, y) {
+    // keyword this
+    // makes variables public on the object
+
+    // public instance variables
     this.x = x;
     this.y = y;
     this.color = color(255, 0, 0);
     this.foodSize = 50;
-    
-    this.display = function() {
+
+    this.display = function () {
         fill(this.color);
         ellipse(this.x, this.y, this.foodSize, this.foodSize);
     }
 }
 
-function Giraffe() {
-    // instance variables
+function Puppy() {
+    // private instance variables
     var x = mouseX;
     var y = mouseY;
     var diameter = 200;
 
-    this.getDistance = function(other) {
+    this.getDistance = function (other) {
         var dist = Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
         return dist;
-    }
-    
-    this.eat = function() {
-        console.log('try to eat');
-        for(var i = 0; i < numFood; i++) {
+    };
+
+    this.eat = function () {
+        for (var i = feed.length - 1; i >= 0; i--) {
             var food = feed[i];
             var d = this.getDistance(food);
             var r1 = food.foodSize / 2;
             var r2 = diameter / 2;
-            if(r1 + r2 > d) {
-                console.log('hit');
+            if (r1 + r2 > d) {
+                feed.splice(i, 1);
+                feed.push(new Food(random(width), random(height)));
             }
         }
-    }
-
+    };
+    
+    
      this.display = function() {
 
         x = mouseX;
